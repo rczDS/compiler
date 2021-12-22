eb41933877e51198eae6f63a8ef579dd7d6da781
Asahi5508 <86864514+Asahi5508@users.noreply.github.com>
2021-12-22 19:33:29 +0800

    Add files via upload

---
+++ testfile/palindrome.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+
+
+int main() {
+	printf("Please input a string:\n");
+	char str[100];
+	gets(str);
+	int len = strlen(str);
+	int flag = 0;
+	for (int i = 0; i < len / 2; i++)
+	{
+		if (str[i] != str[len - 1 - i])
+		{
+			printf("No");
+			flag = 1;
+			break;
+		}
+	}
+	if (!flag) printf("Yes");
+	return 0;
+}
